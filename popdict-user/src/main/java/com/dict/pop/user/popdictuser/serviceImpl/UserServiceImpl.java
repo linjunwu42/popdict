1c68d3b4ad5c75a25e9450b6d654b50060f388c6
linjunwu <404073614@qq.com>
2018-10-23 15:27:57 +0800

    修改

--- popdict-user/src/main/java/com/dict/pop/user/popdictuser/serviceImpl/UserServiceImpl.java
+++ popdict-user/src/main/java/com/dict/pop/user/popdictuser/serviceImpl/UserServiceImpl.java
@@ -1,13 +1,3 @@
-/**
- * copyright (c), 2015-2018, xxx有限公司
- * filename: userserviceimpl
- * author:   lin
- * date:     2018/9/28 16:08
- * description: 登录烦烦烦
- * history:
- * <author>          <time>          <version>          <desc>
- * 作者姓名           修改时间           版本号              描述
- */
 package com.dict.pop.user.popdictuser.serviceImpl;
 
 import com.alibaba.dubbo.config.annotation.Service;
@@ -16,14 +6,6 @@ import com.dict.pop.common.popdictcommon.jiekou.UserService;
 import com.dict.pop.user.popdictuser.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * 〈一句话功能简述〉<br>
- * 〈登录烦烦烦〉
- *
- * @author lin
- *  2018/9/28
- * @since 1.0.0
- */
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -36,7 +18,7 @@ public class UserServiceImpl implements UserService {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        user.setPrikey("933977723");
+        user.setPrikey("930009");
         try {
             flag = userMapper.insertUsers(user);
         }catch (Exception e){
